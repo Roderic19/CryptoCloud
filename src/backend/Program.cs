8208bdb039f110430521c3be402bb3ea10a53ac2
Roderic Bouesse <rbouesse@device-161.home>
2026-05-22 08:59:29 +0200

    feat: scaffold .NET backend and Angular frontend

---
+++ src/backend/Program.cs
@@ -0,0 +1,18 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services.AddOpenTelemetry()
+    .UseFunctionsWorkerDefaults()
+    .UseAzureMonitorExporter(options =>
+        {
+            options.ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+        });
+
+builder.Build().Run();
